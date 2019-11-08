6f077c239ad548694c9a4a526f0ce07300ac6416
Owen Martin <owenscott@gmail.com>
2019-11-08 09:22:31 -0800

    Exercise 7.5

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -2,6 +2,7 @@ package fpinscala.parallelism
 
 import java.util.concurrent._
 import language.implicitConversions
+import scala.annotation.tailrec
 
 object Par {
   type Par[A] = ExecutorService => Future[A]
@@ -16,17 +17,27 @@ object Par {
     def isCancelled = false 
     def cancel(evenIfRunning: Boolean): Boolean = false 
   }
-  
-  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = // `map2` doesn't evaluate the call to `f` in a separate logical thread, in accord with our design choice of having `fork` be the sole function in the API for controlling parallelism. We can always do `fork(map2(a,b)(f))` if we want the evaluation of `f` to occur in a separate thread.
+
+  /** `map2` doesn't evaluate the call to `f` in a separate logical thread, in accord with our design choice of having
+    *  `fork` be the sole function in the API for controlling parallelism. We can always do `fork(map2(a,b)(f))`
+    *  if we want the evaluation of `f` to occur in a separate thread.
+    */
+  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] =
     (es: ExecutorService) => {
-      val af = a(es) 
+      val af = a(es)
       val bf = b(es)
-      UnitFuture(f(af.get, bf.get)) // This implementation of `map2` does _not_ respect timeouts, and eagerly waits for the returned futures. This means that even if you have passed in "forked" arguments, using this map2 on them will make them wait. It simply passes the `ExecutorService` on to both `Par` values, waits for the results of the Futures `af` and `bf`, applies `f` to them, and wraps them in a `UnitFuture`. In order to respect timeouts, we'd need a new `Future` implementation that records the amount of time spent evaluating `af`, then subtracts that time from the available time allocated for evaluating `bf`.
+      UnitFuture(f(af.get, bf.get))
     }
-  
+  // This implementation of `map2` does _not_ respect timeouts, and eagerly waits for the returned futures.
+  // This means that even if you have passed in "forked" arguments, using this map2 on them will make them wait.
+  // It simply passes the `ExecutorService` on to both `Par` values, waits for the results of the Futures `af` and `bf`,
+  // applies `f` to them, and wraps them in a `UnitFuture`. In order to respect timeouts,we'd need a new `Future` implementation
+  // that records the amount of time spent evaluating `af`, then subtracts that time from the available time allocated for evaluating `bf`. And vice-versa?
+
+
   def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
-    es => es.submit(new Callable[A] { 
-      def call = a(es).get
+    (es: ExecutorService) => es.submit(new Callable[A] {
+      override def call = a(es).get
     })
 
   def map[A,B](pa: Par[A])(f: A => B): Par[B] = 
@@ -47,15 +58,18 @@ object Par {
 
   //Ex 7.4
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
   def asyncF[A,B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
 
 
   //ex 7.5 _hard_
   // No additional primitives are required. Do not call run.
-  def sequence[A](ps: List[Par[A]]): Par[List[A]] = {
-
-    ???
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] = ps match {
+    case a1 :: tail =>
+      map2(a1, sequence(tail))((x,y) => x +: y)
+    case a :: Nil => map(a)(a => List(a))
   }
+
 
   //ex 7.6
   def parFilterQ[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
